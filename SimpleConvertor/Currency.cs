2b472358d3bfc1a289d0b52e36b060dee0795362
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-04-27 20:56:13 +0300

    First commit
    Issues:
        - Currency ComboBoxes (or one of them) don`t/doesn`t work properly
            Possible fixes:
                - Change currency choose mechanism (inside "Form1()")
        - UI is rather uncomfortable
            Possible fixes:
                - Make custom buttons, fields, etc.
                - Fix window size
                - Make "Change sides" kind-of button

---
+++ SimpleConvertor/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConvertor
+{
+    class Currency
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public decimal RelationToUAH { get; set; }
+    }
+}
---
+++ SimpleConvertor/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+
+namespace SimpleConvertor
+{
+    public partial class Form1 : Form
+    {
+        delegate void Convertation(string inputString, object presentCurrencyBox, object neededCurrencyBox);
+        event Convertation Convert;
+
+        List<Currency> currencies1 = new List<Currency>();
+        List<Currency> currencies2 = new List<Currency>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            currencies1.Add(new Currency() { Name = "₴", Value = "UAH", RelationToUAH = 1});
+            currencies1.Add(new Currency() { Name = "₽", Value = "RUB", RelationToUAH = 27.80M});
+            currencies1.Add(new Currency() { Name = "$", Value = "USD", RelationToUAH = 0.37M});
+
+            currencies2.Add(new Currency() { Name = "₴", Value = "UAH", RelationToUAH = 1 });
+            currencies2.Add(new Currency() { Name = "₽", Value = "RUB", RelationToUAH = 27.80M });
+            currencies2.Add(new Currency() { Name = "$", Value = "USD", RelationToUAH = 0.37M });
+
+            this.comboBox1.DataSource = currencies1;
+            this.comboBox1.DisplayMember = "Name";
+            this.comboBox1.ValueMember = "Value";
+
+            this.comboBox2.DataSource = currencies2;
+            this.comboBox2.DisplayMember = "Name";
+            this.comboBox2.ValueMember = "Value";
+
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            Convert += ConvertationHandler;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Convert?.Invoke(textBox1.Text, comboBox1.SelectedItem , comboBox2.SelectedItem);
+        }
+
+        private void ConvertationHandler(string inputString, object presentCurrencyBox, object neededCurrencyBox)
+        {
+            decimal inputNumber = 0;
+            try
+            {
+                inputNumber = Decimal.Parse(inputString);
+            }
+            catch (FormatException)
+            {
+                string message = "Input is not formatted right";
+                string title = "Error";
+                MessageBox.Show(message, title);
+                textBox1.Text = "0";
+                return;
+            }
+            (Currency c1, Currency c2) = (currencies1[comboBox1.SelectedIndex], currencies2[comboBox2.SelectedIndex]);
+            decimal coef1 = c1.RelationToUAH;
+            decimal coef2 = c2.RelationToUAH;
+            decimal outputNumber = inputNumber / coef1 * coef2;
+            textBox2.Text = outputNumber.ToString();
+        }
+    }
+}
